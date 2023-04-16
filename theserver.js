f342163830ea411ab353d7b144df865650500827
n00bUltima <alexluca406@gmail.com>
2023-04-17 00:18:32 +0200

    orly

--- theserver.js
+++ theserver.js
@@ -15,7 +15,7 @@ const girlList = require("./girls.json")
 function checkNewDate() {
     year = date.getUTCFullYear()
     month = date.getUTCMonth() + 1
-    day = date.getUTCDate()
+    day = date.getUTCDate() + 99
     dateString = year + " " + month + " " + day
     seed = seedrandom(dateString)
 }
